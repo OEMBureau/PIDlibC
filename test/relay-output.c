8578f5638c02e34d5b0eac701cb7ace771572f48
OEMbureau.com <req@OEMbureau.com>
2019-05-27 16:41:14 +0300

    v.0.0

--- test/relay-output.c
+++ test/relay-output.c
@@ -39,7 +39,7 @@ bool relay_state(
     }
 
   
-    if(*pid->myOutput < (FloatType)(rtc - *windowStartTime)) {
+    if (((*pid->myOutput < (FloatType)(rtc - *windowStartTime)) && (*pid->myOutput > pid->outMin)) || (*pid->myOutput >= pid->outMax)) {
         state = true;
     }else{
         state = false;
@@ -66,10 +66,10 @@ int main(){
     // Real Time counter in msec
     unsigned long rtc = 0; 
     // Specify the links and initial tuning parameters
-    double Kp=2, Ki=5, Kd=1;
+    double Kp=1, Ki=2, Kd=1;
     PidType myPID;
     // Init
-    Setpoint = 25; // Goal of external parameter
+    Setpoint = 25; // Goal of external parameter, you can change it runtime, algorithm accept it
     PID_init(
         &myPID,     // Pid structure 
         &Input,     // Input value
@@ -79,7 +79,7 @@ int main(){
         Ki,         // Integral coeff
         Kd,         // Derivative coeff
         P_ON_E,     // Proportional component on error
-        PID_Direction_Direct, // The deriction is direct (heater, humidifier, etc) 
+        PID_Direction_Reverse, // The deriction is direct (heater, humidifier, etc) 
         &rtc        // Tie with RTC variable
         );
 
@@ -100,10 +100,14 @@ int main(){
     for(int i=0; i<100; i++){// in reality while(1){
         rtc += 1000;
         Input += 3;
+        if(Input > 30) {Input = 20;}
         //printf("\n\rRTC = %lu", rtc);
         relay_state(&myPID, &windowStartTime, MINIMAL_TIME_WINDOW_SEC);
     }
 
+    // If you want manually change output stop PID
+    PID_SetMode(&myPID, PID_Mode_Manual);
+    // for manual change of coefficients use PID_SetTunings
    
 
     return 0;
